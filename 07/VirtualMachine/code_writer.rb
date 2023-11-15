d8e3f34d8c2ee97c165a181e67f139e202a9e800
daiki0381 <daiki03810218@gmail.com>
2023-11-15 20:23:46 +0900

    VM変換器の実装 (算術コマンド)

---
+++ 07/VirtualMachine/code_writer.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+class CodeWriter
+  def initialize(file)
+    @file = File.open(file, 'w')
+  end
+
+  def write_push_pop(command_type, segment, index)
+    if command_type == 'C_PUSH' then
+      if segment == 'constant' then
+        write_codes([
+          "@#{index}",
+          'D=A'
+        ])
+        write_push_from_d_register
+      end
+    end
+  end
+
+  def write_aristhmetic(command)
+    case command
+    when 'add', 'sub', 'and', 'or'
+      write_binary_operation(command)
+    when 'neg', 'not'
+      write_unary_operation(command)
+    end
+  end
+
+  def close
+    @file.close
+  end
+
+  private
+
+  def write_code(code)
+    @file.write("#{code}\n")
+  end
+
+  def write_codes(codes)
+    codes.each do |code|
+      write_code(code)
+    end
+  end
+
+  # Push stack
+  def write_push_from_d_register
+    write_codes([
+      '@SP',
+      'A=M',
+      'M=D',
+      '@SP',
+      'M=M+1'
+    ])
+  end
+
+  # Pop stack
+  def write_pop_to_a_register
+    write_codes([
+      '@SP',
+      'M=M-1',
+      'A=M'
+    ])
+  end
+  
+  def write_binary_operation(command)
+    write_pop_to_a_register
+    write_code('D=M')
+    write_pop_to_a_register
+    case command
+    when 'add' then
+      write_code('D=D+M')
+    when 'sub'
+      write_code('D=M-D')
+    when 'and'
+      write_code('D=D&M')
+    when 'or'
+      write_code('D=D|M')
+    end
+    write_push_from_d_register
+  end
+
+  def write_unary_operation(command)
+    write_codes([
+      '@SP',
+      'A=M-1'
+    ])
+    case command
+    when 'neg' then
+      write_code('M=-M')
+    when 'not'
+      write_code('M=!M')
+    end
+  end
+end
---
+++ 07/VirtualMachine/virtual_machine.rb
@@ -0,0 +1,36 @@
+# !/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative './code_writer'
+require_relative './parser'
+
+class VirtualMachine
+  def execute
+    file = ARGV[0]
+    if file.end_with?('.vm') then
+      code_writer = CodeWriter.new(file.gsub(/\.vm$/, '.asm'))
+      translate_file(file, code_writer)
+    end
+  end
+
+  private
+
+  def translate_file(file, code_writer)
+    parser = Parser.new(file)
+    while parser.has_more_commands?
+      parser.advance
+
+      next if parser.command.empty?
+
+      case parser.command_type
+      when 'C_ARITHMETIC'
+        code_writer.write_aristhmetic(parser.arg1)
+      when 'C_PUSH', 'C_POP'
+        code_writer.write_push_pop(parser.command_type, parser.arg1, parser.arg2)
+      end
+    end
+    code_writer.close
+  end
+end
+
+VirtualMachine.new.execute
